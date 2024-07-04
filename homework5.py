e95911b8cd852f210d6b1d7240fd70242ebd4e8d
Spira302 <egortretyakov2020@gmail.com>
2024-07-04 13:43:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, 2, 3, "string", True)
+print(immutable_var)
+mutable_list = [1, 2, 'a', 'b', 'Modified']
+print(mutable_list)
+mutable_list [1]= 44
+mutable_list [3]= 52
+mutable_list [4]= 'Saint_Petersburg'
+print(mutable_list)
